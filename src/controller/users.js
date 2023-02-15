e89f0ebfee3e5ed7bf24ef16e5804b365b44b090
baha-udin <mbaha713m@gmail.com>
2023-02-15 17:56:53 +0700

    add models & controller for update & delete users

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,52 +1,87 @@
-const userModel = require('./../models/users')
+const userModel = require("./../models/users");
 
 // because get data is asynchronous we use async await
 const getAllUsers = async (req, res) => {
-    try {
-    // in data, we use array because default response data mysql2 is: rows & fields, & we don't need fields
-        const [data] = await userModel.getAllUsers() // so, i just rename [rows] => became [data]
-        res.json({
-            message: "GET All Users Success",
-            data: data
-        });
-    } catch (error) {
-        res.status(500);
-        res.json({
-            message: 'Failed to connect database',
-            serverMessage: `${error}`
-        })
-    }
-}
-
-const createNewUsers = (req, res) => {
-    console.log(req.body)
+  try {
+    // in [data], we use array because default response data mysql2 is: rows & fields, & we don't need fields
+    const [data] = await userModel.getAllUsers(); // so, i just rename [rows] => became [data]
     res.json({
-        message: "CREATE NEW Users Success",
-         data: req.body
-    })
-}
+      message: "GET All Users Success",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      message: "Failed to connect database",
+      serverMessage: `${error}`,
+    });
+  }
+};
 
-const updateUser = (req, res) => {
-    const {idUser} = req.params
-    console.log(`id user: ${idUser}`)
+const createNewUsers = async (req, res) => {
+  const { body } = req;
+
+  if(!body.name || !body.email || !body.address) {
+    return res.status(400).json({
+        message: 'Data input was wrong or empty, please re-check again',
+        data: null
+    })
+  }
+
+  try {
+    await userModel.createNewUser(body);
+    res.status(201);
+    res.json({
+      message: "CREATE NEW Users Success",
+      data: req.body,
+    });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      message: "Failed to connect database",
+      serverMessage: `${error}`,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  const { idUser } = req.params;
+  const {body} = req
+  
+  try {
+    await userModel.updateUser(body, idUser)
+    res.status(201);
     res.json({
         message: "update user was success",
-        id: idUser,
-        data: req.body
-    })
-}
-
-const deleteUser = (req, res) => {
-    const {idUser} = req.params
-    res.json({
-        message: "deleted user was success",
         data: {
             id: idUser,
-            name: "prawito",
-            email: "prawito@email.com"
-
+            ...body
         }
-    })
-}
+      });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      message: "Failed to connect database",
+      serverMessage: `${error}`,
+    });
+  }
+};
 
-module.exports = {getAllUsers, createNewUsers, updateUser, deleteUser}
+const deleteUser = async (req, res) => {
+  const { idUser } = req.params;
+  try {
+    await userModel.deleteUser(idUser)
+    res.json({
+        message: "deleted user was success",
+        data: null
+      });
+  } catch (error) {
+    res.status(500);
+    res.json({
+      message: "Failed to connect database",
+      serverMessage: `${error}`,
+    });
+  }
+};
+
+module.exports = { getAllUsers, createNewUsers, updateUser, deleteUser };
--- src/models/users.js
+++ src/models/users.js
@@ -1,10 +1,33 @@
 const dbPool = require('./../config/database')
 
-// CREATE Models query: get All users from database
+// Model query: get All users from database
 const getAllUsers = (req, res) => {
     const SQLQuery = 'SELECT * from users' 
+    //dbPool.execute is syntax from mysql2 to connect db
+    return dbPool.execute(SQLQuery)
+}
+
+// Model query: create new users to database
+const createNewUser = (body) => {
+    const SQLQuery = `INSERT INTO users (name, email, address) 
+                    VALUES ('${body.name}', '${body.email}', '${body.address}')`
 
     return dbPool.execute(SQLQuery)
 }
 
-module.exports = {getAllUsers}
+// Model query: update data users
+const updateUser = (body, idUser) => {
+    const SQLQuery = `UPDATE users 
+                    SET name='${body.name}', email='${body.email}', address='${body.address}'
+                    WHERE id=${idUser} `
+    return dbPool.execute(SQLQuery)
+}
+
+// Model query: delete users
+const deleteUser = (iduser) => {
+    const SQLQuery = `DELETE FROM users WHERE id='${iduser}'`
+
+    return dbPool.execute(SQLQuery)
+}
+
+module.exports = {getAllUsers, createNewUser, updateUser, deleteUser}
